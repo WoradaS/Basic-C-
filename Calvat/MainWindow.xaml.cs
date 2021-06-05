260a0260f9dd9ebe1d9b362f40fca14ad0ea80e8
WoradaS <83392682+WoradaS@users.noreply.github.com>
2021-06-05 16:42:02 +0700

    Add files via upload

---
+++ Calvat/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StaticTestApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double price = double.Parse(PriceTextBox.Text);
+            double vat = double.Parse(VatTextBox.Text);
+
+            double totalprice = VatCal.TotalPrice(price, vat);
+            double vatprice = VatCal.VatPrice(price, vat);
+
+            MessageBox.Show("ราคาสินค้า: " + price + " บาท ภาษี: " + vat + "% \n"
+                + "ภาษีมูลค่าเพิ่ม: " + vatprice + " บาท ราคาทั้งหมด: " + totalprice + " บาท");
+        }
+
+        
+    }
+}
---
+++ Calvat/VatCal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticTestApp
+{
+    class VatCal
+    {
+        public static double TotalPrice(double price,double vat)
+        {
+            
+            return (price * (vat / 100)) + price;
+        }
+        public static double VatPrice(double price,double vat)
+        {
+            return ( price * vat ) / 100;
+        }
+    }
+}
