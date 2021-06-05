d931c60527c3ef881971a882051708d2a9e30b5d
WoradaS <83392682+WoradaS@users.noreply.github.com>
2021-06-05 17:14:51 +0700

    Add files via upload

---
+++ Employee/Empolyee.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_WPF
+{
+    class Empolyee
+    {
+        // Access Modifier:  Public / Private  / Internal / Protected
+
+        // Field = ตัวแปรที่ใช้งานภายในคลาส
+        private int _id;
+        private string _name;
+        private string _lastname;
+        private string _position;
+
+        // Overloading Method
+        public Empolyee(int id, string name , string lastname , string position)
+        {
+            Id = id;
+            Name = name;
+            Lastname = lastname;
+            Position = position;
+        }
+        // this() = การเรียกใช้งาน constructor
+        public Empolyee(int id, string name, string lastname):this()
+        {
+            Id = id;
+            Name = name;
+            Lastname = lastname;
+            
+        }
+        public Empolyee()
+        {
+            Id = 0;
+            Name = "Not find";
+            Lastname = "Not find";
+            Position = "Not find";
+        }
+
+        // Encapsulaton = การห่อหุ้ม
+        private string FullName()
+        {
+            return Name + " " + Lastname;
+        }
+        // Property          
+        public int Id
+        {
+            get {
+                return _id;
+            }
+            set{
+                _id = value;
+            }
+        }
+        public string Name { get => _name; set => _name = value; }
+        public string Lastname { get => _lastname; set => _lastname = value; }
+        public string Position { get => _position; set => _position = value; }
+    }
+}
---
+++ Employee/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_WPF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {   // Property ระดับ obj ต้องสร้าง obj ขึ้นมาก่อน
+            Empolyee emp1 = new Empolyee(1, "Frist", "L", "CEO");
+         /*   emp1.Id = 1;
+            emp1.Name = "Frist";
+            emp1.Lastname = "L";
+            emp1.Position = "CEO"; */
+
+            Empolyee emp2 = new Empolyee(2, "song", "L", "Engineer");
+          /*  emp2.Id = 2;
+            emp2.Name = "song";
+            emp2.Lastname = "L";
+            emp2.Position = "Engineer"; */
+
+            Empolyee emp3 = new Empolyee(3, "tree", "L", "Web Dev");
+            Empolyee emp4 = new Empolyee(4, "for", "L", "Ux/Ui");
+            Empolyee emp5 = new Empolyee(5,"ha","L");
+
+            List<Empolyee> list = new List<Empolyee>();
+            list.Add(emp1);
+            list.Add(emp2);
+            list.Add(emp3);
+            list.Add(emp4);
+            list.Add(emp5);
+
+            // loop for Array,list C#
+            foreach (Empolyee obj in list)
+            {
+                if (obj.Id == int.Parse(employeeidTextbox.Text))
+                {
+                    MessageBox.Show("id: " + obj.Id + " Name: " + obj.Name + " LastName: " + obj.Lastname + " Position: " + obj.Position);
+                    break;
+                }
+
+            }
+        }
+    }
+}
